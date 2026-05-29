b7c4ed524448aaae6534bd3ed24225108d59f6c6
billyelcalvo <dam0806blas@gmail.com>
2026-05-29 00:34:44 -0500

    chore: modify table users and encapsulate prismaService.ts

--- iam/infrastructure/persistence/prisma/repository/userRepository.ts
+++ iam/infrastructure/persistence/prisma/repository/userRepository.ts
@@ -1,17 +1,28 @@
-import {UserInterface} from '../../../../domain/schemas/user.schema'
-import {User} from '../../../../domain/entities/user'
-import {hashService} from '../../bcrypt/service/hashService'
+import { UserInterface } from "../../../../domain/schemas/user.schema";
+import { User } from "../../../../domain/entities/user";
+import { hashService } from "../../bcrypt/service/hashService";
+import { prismaService } from "../../prisma/service/prismaService";
 
-export  async function createUser(user : UserInterface){
+export class userRepository {
+
+  private static client = prismaService.getClient();
+
+  static async createUser(user: UserInterface) {
     const password = await hashService.hash(user.password);
+
+    await this.client.user.create({
+      data: {
+        name : user.name,
+        password : password
+      }
+
+    });
+
     return new User(user.name, password, user.email);
-}
-export function updateUser(user : UserInterface){
-
-}
-export function getById(id : string){
-
-}
-export function getByEmail(email : string){
-
+  }
+  static async updateUser(user: UserInterface) {
+    
+  }
+  static async getById(id: string) {}
+  static async getByEmail(email: string) {}
 }
--- iam/infrastructure/persistence/prisma/service/prismaService.ts
+++ iam/infrastructure/persistence/prisma/service/prismaService.ts
@@ -1,13 +1,23 @@
 import { PrismaClient } from '../../../../../generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+export class prismaService{
+    private static globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+    private static instance : PrismaClient
+    
+    static getClient(){
+        if(!this.instance){
+            this.instance = this.globalForPrisma.prisma?? new PrismaClient(
+            {adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL })}
+            );
+             this.globalForPrisma.prisma = this.instance;
+        }
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient(
-    {adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL })}
-);
+        return this.instance;
+    }
+    
+}
 
-if(process.env.NODE_ENV !== 'production')  globalForPrisma.prisma = prisma;
 
-export default prisma;
+
 
