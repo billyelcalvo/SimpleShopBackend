66a843df5331e508dd35baa0398520dfc17a10ee
billyelcalvo <dam0806blas@gmail.com>
2026-06-06 20:49:36 -0500

    chore: add endpoint on server.ts

--- iam/interfaces/assembler/userAssembler.ts
+++ iam/interfaces/assembler/userAssembler.ts
@@ -1,8 +1,8 @@
 import {UserSchema} from '../../domain/schemas/user.schema'
 
 export class userAssembler{
-    static rawToInterface({name, password, email} : {name : string, password : string, email? : string}){
-        const result = UserSchema.safeParse({ name, password, email}); 
+    static rawToInterface({name, password, email, id} : {name : string, password : string, email? : string, id? : string}){
+        const result = UserSchema.safeParse({ name, password, email, id}); 
 
         if(!result.success){
             throw new Error(result.error.message)
--- iam/interfaces/controller/userController.ts
+++ iam/interfaces/controller/userController.ts
@@ -7,12 +7,24 @@ const router = Router();
 router.get("/",(req : Request, res : Response)=>{
     return res.status(200).json(userRepository.getAll());
 })
+
 router.get("/:id",(req : Request, res : Response)=> {
     const id = Array.isArray(req.params.id) ? req.params.id[0] : req.params.id ;
     return res.status(200).json(userRepository.getById(id));
 });
+
 router.post("/",(req : Request, res : Response) =>{
     const userSchema = userAssembler.rawToInterface(req.body);
-    
-    return res.status(200).json();
-})
+    const user = userRepository.createUser(userSchema);
+
+    return res.status(200).json(user);
+});
+
+router.put("/", (req : Request, res : Response) => {
+    const userSchema = userAssembler.rawToInterface(req.body);
+    const user = userRepository.updateUser(userSchema);
+
+    return res.status(200).json(user);
+});
+
+export default router;
